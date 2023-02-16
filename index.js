93dc3854e81de4cfac90007c9b5f8dc92363b9e1
FarruxDEV <farruxbeknozimov@gmail.com>
2023-02-16 06:09:30 +0500

    use api google serach

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import { Telegraf } from "telegraf";
 import fetch from "node-fetch";
 const TOKEN = "5738705161:AAF15n4PDuO1mkOZ1ZMaSBbLCXZRGA6jEpI";
 import { createClient } from "pexels";
+import imageSearch from "image-search-google";
 
-const client = createClient(
-	"esXMiq8unRbSpR84z13VkwvY4MNqdfqkThYVCIE2tpQWYjsNmM4bOYMr"
+const client = new imageSearch(
+	"1017b26965d7849cd",
+	"AIzaSyBa5minfOP2mpq8RDZLa13K-8aasA0HmyI"
 );
 
 const bot = new Telegraf(TOKEN);
@@ -16,26 +18,20 @@ bot.start((ctx) => {
 
 bot.on("message", async (ctx) => {
 	let text = ctx.update.message.text;
-	console.log(text);
-	const query = "Nature";
 
-	// client.photos.search({ query, per_page: 3 }).then((photos) => {
-	// 	// console.log(photos);
-	// 	let photo = photos.photos;
-	// 	for (let i in photo) {
-	// 		ctx.replyWithPhoto(photo[i].src.original);
-	// 	}
-	// });
-	fetch(`https://api.pexels.com/v1/search?query=${text}`)
-		.then((res) => res.json())
-		.then((json) => {
-			let photos = json.photos;
-			for (let i in photos) {
-				ctx.replyWithPhoto(photos[i].webformatURL);
-			}
-			console.log(json, text);
-		})
-		.catch((err) => console.log(err));
+	const options = { page: 1 };
+	try {
+		client
+			.search(text, options)
+			.then((images) => {
+				for (let i in images) {
+					ctx.replyWithPhoto(images[i].url);
+				}
+			})
+			.catch((error) => console.log(error));
+	} catch (error) {
+		console.log("XATOLIK KETDI");
+	}
 });
 
 bot.launch();

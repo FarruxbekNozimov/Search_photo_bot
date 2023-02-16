fd8ede2ba762cf17c10addd2dfd1c31da8d7a91c
FarruxDEV <farruxbeknozimov@gmail.com>
2023-02-16 08:13:15 +0500

    Remove some packages

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import { Telegraf } from "telegraf";
-import fetch from "node-fetch";
 const TOKEN = "5738705161:AAF15n4PDuO1mkOZ1ZMaSBbLCXZRGA6jEpI";
-import { createClient } from "pexels";
 import imageSearch from "image-search-google";
+
+const bot = new Telegraf(TOKEN);
 
 const client = new imageSearch(
 	"1017b26965d7849cd",
 	"AIzaSyBa5minfOP2mpq8RDZLa13K-8aasA0HmyI"
 );
-
-const bot = new Telegraf(TOKEN);
 
 bot.start((ctx) => {
 	const firstName = ctx.update.message.from.first_name;
@@ -18,17 +16,20 @@ bot.start((ctx) => {
 
 bot.on("message", async (ctx) => {
 	let text = ctx.update.message.text;
-
-	const options = { page: 1 };
+	console.log(text);
 	try {
+		const options = { page: 1 };
 		client
 			.search(text, options)
 			.then((images) => {
+				console.log(images);
 				for (let i in images) {
+					console.log(images[i].url.slice(images[i].url.lastIndexOf(".")));
+					console.log(images[i].url.slice(-images[i].url.lastIndexOf(".")));
 					ctx.replyWithPhoto(images[i].url);
 				}
 			})
-			.catch((error) => console.log(error));
+			.catch((error) => console.log("XATO"));
 	} catch (error) {
 		console.log("XATOLIK KETDI");
 	}
